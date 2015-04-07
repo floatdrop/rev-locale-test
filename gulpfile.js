bc81d948077839bb0560921b0140338acd63130f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-04-07 22:32:32 +0500

    Fix stuff

--- gulpfile.js
+++ gulpfile.js
@@ -19,9 +19,8 @@ $.vinylPaths = require('vinyl-paths');
 // Clean /dist dir & rev manifest
 // ----------------------------------------------
 
-clean = function(){
-  gulp.src( ["./dist/*"], {read: false} )
-    .pipe( $.vinylPaths($.del) );
+clean = function(cb){
+  $.del("./dist", cb);
 };
 gulp.task( "clean", clean );
 
@@ -51,48 +50,60 @@ gulp.task( "clean", clean );
 // @uberspeck's broken fix attempt
 // ----------------------------------------------
 
-// gulp.task( "concatLocales", function(){
-//   var fileName, i, len, locale, locales;
-//   locales = ["en-us", "en-ca", "fr-CA"];
-//   for (i = 0, len = locales.length; i < len; i++) {
-//     locale = locales[i];
-//     fileName = locale + ".json";
-//     gulp.src( "./src/**/"+locale+".json" )
-//       .pipe( $.concat( {path:fileName, cwd:"./"}, {newLine:","} ) )
-//       .pipe( $.wrap("{ <%= contents %> }", {}, {parse:false}) )
-//       .pipe( $.jsonminify() )
-//       .pipe( $.rev() )
-//       .pipe( gulp.dest( "./dist/locales" ) );
-//   }
-// });
-// gulp.task( "createLocaleManifest", function(){
-//   gulp.src( "./dist/locales/*.json" )
-//     .pipe( $.rev.manifest( {path:"locales.json", merge:true} ) )
-//     .pipe( gulp.dest( "dist/rev" ) );
-// });
-// gulp.task( "locales", function(){
-//   $.runSequence( "concatLocales", "createLocaleManifest" );
-// });
+gulp.task( "concatLocales", function(cb){
+  var fileName, i, len, locale, locales;
+  locales = ["en-us", "en-ca", "fr-CA"];
+  var streamsDone = 0;
+  for (i = 0, len = locales.length; i < len; i++) {
+    locale = locales[i];
+    fileName = locale + ".json";
+        gulp.src( "./src/**/"+locale+".json" )
+            .pipe( $.concat( {path:fileName, cwd:"./"}, {newLine:","} ) )
+            .pipe( $.wrap("{ <%= contents %> }", {}, {parse:false}) )
+            .pipe( $.jsonminify() )
+            .pipe( $.rev() )
+            .pipe( gulp.dest( "./dist/locales" ) )
+            .on('end', function () {
+                streamsDone ++;
+                if (streamsDone >= locales.length) {
+                    cb();
+                }
+            });
+  }
+
+});
+
+gulp.task( "createLocaleManifest", function(){
+  return gulp.src( "./dist/locales/*.json" )
+    .pipe( $.rev() )
+    .pipe( $.rev.manifest() )
+    .pipe( gulp.dest( "dist" ) );
+});
+gulp.task( "locales", function(cb){
+  $.runSequence( "concatLocales", "createLocaleManifest", cb);
+});
 
 
 // @floatdrop's initial suggested solution
 // ----------------------------------------------
-
-gulp.task( "locales", function(){
-  gulp.src( "./src/**/{en-us,en-ca,fr-ca}.json" )
-    .pipe( $.concat( {path:'concated-locales.json', cwd:"./"}, {newLine:","} ) )
-    .pipe( $.wrap("{ <%= contents %> }", {}, {parse:false}) )
-    .pipe( $.jsonminify() )
-    .pipe( $.rev() )
-    .pipe( gulp.dest( "./dist/locales" ) )
-    .pipe( $.rev.manifest( {base: "/dist/rev", path:"locales.json", merge:true} ) )
-    .pipe( gulp.dest( "dist/rev" ) );
-});
+//
+// gulp.task( "locales", function(){
+//     var locales = ["en-us", "en-ca", "fr-CA"];
+//     for (var )
+//   gulp.src( "./src/**/.json" )
+//     .pipe( $.concat( {path:'concated-locales.json', cwd:"./"}, {newLine:","} ) )
+//     .pipe( $.wrap("{ <%= contents %> }", {}, {parse:false}) )
+//     .pipe( $.jsonminify() )
+//     .pipe( $.rev() )
+//     .pipe( gulp.dest( "./dist/locales" ) )
+//     .pipe( $.rev.manifest( {base: "./dist/rev", path:"locales.json", merge:true} ) )
+//     .pipe( gulp.dest( "dist/rev" ) );
+// });
 
 // ----------------------------------------------
 // Clean & Rebuild
 // ----------------------------------------------
-defaultTask = function(){
-  $.runSequence("clean", "locales");
+defaultTask = function(cb){
+  $.runSequence("clean", "locales", cb);
 };
 gulp.task( "default", defaultTask );
